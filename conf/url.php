49fd3b32e81554daa2bf42ad270266477ed5c88e
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2016-11-30 10:05:47 +0100

    url update

---
+++ conf/url.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebastiao.lutonda
+ * Date: 30-11-2016
+ * Time: 10:05
+ */
+
+$atual = (isset($_GET['isw'])) ? $_GET['isw'] : 'home';
+$pasta = 'paginas';
+if (substr_count($atual, '/') > 0) {
+    $atual = explode('/', $atual);
+    $pagina = (file_exists("{$pasta}/" . $atual[0] . '.php')) ? $atual[0] : 'erro';
+    $id = $atual[1];
+    $busca = @$atual[2];
+
+} else {
+    $pagina = (file_exists("{$pasta}/" . $atual . '.php')) ? $atual : 'erro';
+    $id = 0;
+    $frame=0;
+}?>
