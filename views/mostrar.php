9c7eef748d65bf76b45071ffbb38ed1b9956fe09
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2016-11-30 17:10:21 +0100

    criação do views     - impletemntação do login/views

--- views/mostrar.php
+++ views/mostrar.php
@@ -1,3 +1,21 @@
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.common.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.common.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.common.min.js.map"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.css"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.min.css"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.1.1/sweetalert2.min.js"></script>
+    <script>
+        swal('Hello world!')
+    </script>
+</head>
+
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
