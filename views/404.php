c82c8c9a79e9cc3b4d4efe279711530fd5cb1038
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2016-11-30 15:58:36 +0100

    criação do views     - impletemntação do login/views

--- views/404.php
+++ views/404.php
@@ -1,9 +1,52 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sebastiao.lutonda
- * Date: 30-11-2016
- * Time: 15:03
- */
+<!DOCTYPE html>
+<!-- saved from url=(0047)http://getbootstrap.com/examples/sticky-footer/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-echo '404';
+    <title>Sticky Footer Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="views/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="views/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="views/assets/css/sticky-footer.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="views/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="views/assets/js/ie-emulation-modes-warning.js.transferir"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <link id="noteanywherecss" media="screen" type="text/css" rel="stylesheet" href="data:text/css,.note-anywhere%20.closebutton%7Bbackground-image%3A%20url%28chrome-extension%3A//bohahkiiknkelflnjjlipnaeapefmjbh/asset/deleteButton.png%29%3B%7D%0A"></head>
+
+  <body><span class="viber-share-selection" id="viber2" style="position: absolute; top: 0px; left: 0px; width: 24px; height: 23px; background-size: 100%; display: none; z-index: 2147483647; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/text-button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span>
+
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>Sticky footer</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+      <p>Use <a href="http://getbootstrap.com/examples/sticky-footer-navbar">the sticky footer with a fixed navbar</a> if need be, too.</p>
+    </div>
+
+    <?php include('views/assets/api/footer.php')?>
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="views/assets/js/ie10-viewport-bug-workaround.js.transferir"></script>
+  
+
+<span class="viber-share" id="viber1" style="position: absolute; top: 0px; left: 0px; width: 52px; height: 21px; background-size: 52px 21px; display: none; z-index: 2147483646; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span></body></html>
--- views/inicio.php
+++ views/inicio.php
@@ -1,11 +1,92 @@
+<!DOCTYPE html>
+<!-- saved from url=(0054)http://getbootstrap.com/examples/sticky-footer-navbar/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="views/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="views/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="views/assets/css/sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="views/assets/js/ie-emulation-modes-warning.js.transferir"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <link id="noteanywherecss" media="screen" type="text/css" rel="stylesheet" href="data:text/css,.note-anywhere%20.closebutton%7Bbackground-image%3A%20url%28chrome-extension%3A//bohahkiiknkelflnjjlipnaeapefmjbh/asset/deleteButton.png%29%3B%7D%0A"></head>
+
+  <body><span class="viber-share-selection" id="viber2" style="position: absolute; top: 0px; left: 0px; width: 24px; height: 23px; background-size: 100%; display: none; z-index: 2147483647; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/text-button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://getbootstrap.com/examples/sticky-footer-navbar/#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">INICIO</a></li>
+            <li><a href="/sobre">SOBRE</a></li>
+            <li><a href="/contacto">CONTACTOS</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>Sticky footer with fixed navbar</h1>
+      </div>
+      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+      <p>Back to <a href="http://getbootstrap.com/examples/sticky-footer">the default sticky footer</a> minus the navbar.</p>
+        <hr>
+
+        <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading">Panel heading</div>
+
+            <!-- Table -->
+            <table class="table">
+                <tr><th>3</th></tr>
+                <tr><td>2</td></tr>
+            </table>
+        </div>
+    </div>
+
+  <?php include('views/assets/api/footer.php')?>
 
 
-<?php
-/**
- * Created by PhpStorm.
- * User: sebastiao.lutonda
- * Date: 30-11-2016
- * Time: 14:27
- */
-echo 'inicio';
-echo $data;
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="views/assets/js/jquery.min.js.transferir"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="views/assets/js/bootstrap.min.js.transferir"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="views/assets/js/ie10-viewport-bug-workaround.js.transferir"></script>
+  
+
+<span class="viber-share" id="viber1" style="position: absolute; top: 0px; left: 0px; width: 52px; height: 21px; background-size: 52px 21px; display: none; z-index: 2147483646; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span></body></html>
