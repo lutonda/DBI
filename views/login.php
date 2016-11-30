af6cc8b3d6c8bf1faad14fa0d6105ac7a8d2bf88
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2016-11-30 15:23:19 +0100

    criação do views     - impletemntação do login/views

---
+++ views/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!-- saved from url=(0040)http://getbootstrap.com/examples/signin/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="views/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="views/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="views/assets/css/signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="views/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="views/assets/js/ie-emulation-modes-warning.js.transferir"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <link id="noteanywherecss" media="screen" type="text/css" rel="stylesheet" href="data:text/css,.note-anywhere%20.closebutton%7Bbackground-image%3A%20url%28chrome-extension%3A//bohahkiiknkelflnjjlipnaeapefmjbh/asset/deleteButton.png%29%3B%7D%0A"></head>
+
+  <body><span class="viber-share-selection" id="viber2" style="position: absolute; top: 0px; left: 0px; width: 24px; height: 23px; background-size: 100%; display: none; z-index: 2147483647; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/text-button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Faça Login</h2>
+        <label for="inputEmail" class="sr-only">E-mail ou username</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="E-mail ou username" required="" autofocus="">
+        <label for="inputPassword" class="sr-only">Palavrapasse</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Palavrapasse" required="">
+        <div class="checkbox">
+          <label>
+            <hr>
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="views/assets/js/ie10-viewport-bug-workaround.js.transferir"></script>
+  
+
+<span class="viber-share" id="viber1" style="position: absolute; top: 0px; left: 0px; width: 52px; height: 21px; background-size: 52px 21px; display: none; z-index: 2147483646; background-image: url(&quot;chrome-extension://dafalpmmoljglecaoelijmbkhpdoobmm/images/button@2x.png&quot;); cursor: pointer; background-repeat: no-repeat;"></span></body></html>
